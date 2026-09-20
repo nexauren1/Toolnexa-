0f69f5af26251a5fd07e9e3b5f9d74817142794c
nexauren1 <nexaurenstore@gmail.com>
2026-09-20 20:58:39 +0200

    Bump ToolNexa to version 1.21.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.toolnexa.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "1.20.0"
+        versionCode = 30
+        versionName = "1.21.0"
     }
 
     buildFeatures {
