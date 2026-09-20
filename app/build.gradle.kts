ba35a3851c9165443b20cedb3b33043b2059b83d
nexauren1 <nexaurenstore@gmail.com>
2026-09-20 13:39:09 +0200

    release: ToolNexa Android 1.10.3 billing fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.toolnexa.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.10.2"
+        versionCode = 16
+        versionName = "1.10.3"
     }
 
     buildFeatures {
