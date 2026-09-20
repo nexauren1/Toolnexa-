bd6d7f4ec034047349319e4c1341e88fbe98b7ea
nexauren1 <nexaurenstore@gmail.com>
2026-09-20 08:22:18 +0200

    fix: import bitmap preview dependency

--- app/src/main/java/com/toolnexa/app/MainActivity.kt
+++ app/src/main/java/com/toolnexa/app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.toolnexa.app
 
-import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.net.Uri
