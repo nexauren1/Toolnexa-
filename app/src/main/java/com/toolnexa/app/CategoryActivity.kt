728042783ac258d899754ace878bb0e692b21b24
nexauren1 <nexaurenstore@gmail.com>
2026-09-20 18:54:13 +0200

    fix: import Space for tool grid layout

--- app/src/main/java/com/toolnexa/app/CategoryActivity.kt
+++ app/src/main/java/com/toolnexa/app/CategoryActivity.kt
@@ -14,6 +14,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ScrollView
+import android.widget.Space
 import android.widget.TextView
 import kotlin.math.roundToInt
 
