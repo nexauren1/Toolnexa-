c41ebb480a1152aea1360e0a364f4937f3d06c5e
nexauren1 <nexaurenstore@gmail.com>
2026-09-20 19:03:46 +0200

    release: bump Android app to 1.13.1 stability fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.toolnexa.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.13.0"
+        versionCode = 21
+        versionName = "1.13.1"
     }
 
     buildFeatures {
