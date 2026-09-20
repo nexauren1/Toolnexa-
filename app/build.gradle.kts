f6c50586d3c62c59daaa5b94bcdc282c3b82e264
nexauren1 <nexaurenstore@gmail.com>
2026-09-20 12:16:27 +0200

    release: ToolNexa 1.8.1 billing account link

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.toolnexa.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.8.0"
+        versionCode = 11
+        versionName = "1.8.1"
     }
 
     buildFeatures {
