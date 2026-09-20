426835c208ff6961f93e1c146cdf23fd4be6fe96
nexauren1 <nexaurenstore@gmail.com>
2026-09-20 19:14:02 +0200

    feat: track every app page in Firebase Analytics

---
+++ app/src/main/java/com/toolnexa/app/ToolNexaApplication.kt
@@ -0,0 +1,75 @@
+package com.toolnexa.app
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+class ToolNexaApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        registerActivityLifecycleCallbacks(
+            object :
+                ActivityLifecycleCallbacks {
+                override fun onActivityResumed(
+                    activity: Activity
+                ) {
+                    val name =
+                        activity
+                            .javaClass
+                            .simpleName
+
+                    if (
+                        name == "LoginActivity"
+                    ) {
+                        return
+                    }
+
+                    AnalyticsTracker(
+                        activity
+                    ).screen(
+                        "page_" +
+                            name
+                                .removeSuffix(
+                                    "Activity"
+                                )
+                                .replace(
+                                    Regex(
+                                        "([a-z])([A-Z])"
+                                    ),
+                                    "$1_$2"
+                                )
+                                .lowercase()
+                    )
+                }
+
+                override fun onActivityCreated(
+                    activity: Activity,
+                    savedInstanceState: Bundle?
+                ) = Unit
+
+                override fun onActivityStarted(
+                    activity: Activity
+                ) = Unit
+
+                override fun onActivityPaused(
+                    activity: Activity
+                ) = Unit
+
+                override fun onActivityStopped(
+                    activity: Activity
+                ) = Unit
+
+                override fun onActivitySaveInstanceState(
+                    activity: Activity,
+                    outState: Bundle
+                ) = Unit
+
+                override fun onActivityDestroyed(
+                    activity: Activity
+                ) = Unit
+            }
+        )
+    }
+}
